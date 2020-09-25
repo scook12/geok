143f2747c6611a11a25f37e29e6e5b6350c55246
Samuel Cook <scook@esri.com>
2020-09-25 13:16:15 -0500

    fix: i don't want to talk about it

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_USERNAME" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push cooksamuel/geok_test:$VERSION
 docker push cooksamuel/geok_test:latest
