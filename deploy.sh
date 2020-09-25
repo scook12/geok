544ebf28f1a69e17e4cfd372eca2e314f1cf4145
Samuel Cook <scook@esri.com>
2020-09-25 14:51:50 -0500

    fix: inline poetry build/publish in after_success

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-docker exec $ID poetry config pypi-token.pypi $POETRY_PYPI_TOKEN_PYPI
-docker exec $ID poetry publish --build
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push cooksamuel/geok_test:$VERSION
 docker push cooksamuel/geok_test:latest
